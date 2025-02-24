108302f4ccfa727e55d36ab425b20bffce5ad575
Isaac <isaac@Isaacs-iMac.local>
2025-02-24 13:57:06 -0500

    Intial Commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const sentence = "I love my toys hungry"; 
+
+function longestWord(sentence) {
+  let words = sentence.split(" "); // Split sentence into words
+  let longest = ""; // Variable to store the longest word
+
+  for (let word of words) {
+    if (word.length > longest.length) {
+      longest = word; // Update longest if current word is longer
+    }
+  }
+
+  return longest; // Return the longest word
+}
+
+console.log(LongestWord(sentence)); // Output: "hungry"
